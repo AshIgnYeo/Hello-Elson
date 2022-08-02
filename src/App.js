bc54eaf67a85e54e1f1311e746642214ebee81bd
Ashley Yeo <contact@ashleyignatius.com>
2022-08-02 18:12:00 +0800

    added greeting to heaeder

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Elson</h1>
         <p>
-          Hello Elson! You can view this from the <strong>web</strong>
+          You can view this from the <strong>web</strong>
         </p>
         <a
           className="App-link"
